1c0a742392887d14c17d7058e022a1b313599cae
Luis MX <ing.arellano@gmail.com>
2026-02-03 15:43:47 -0300

    4.6. Implementando a camada de domínio para Delivery a 4.10. Rich Domain Model: Métodos que demonstram intenções de mudança no domínio

---
+++ Microservices/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/domain/model/Delivery.java
@@ -0,0 +1,142 @@
+package com.algaworks.algadelivery.delivery.tracking.domain.model;
+
+import com.algaworks.algadelivery.delivery.tracking.domain.model.exception.DomainException;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.OffsetDateTime;
+import java.util.*;
+
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Setter(AccessLevel.PRIVATE)
+@Getter
+public class Delivery {
+
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    private UUID courierId;
+
+    private DeliveryStatus status;
+
+    private OffsetDateTime placedAt;
+    private OffsetDateTime assignedAt;
+    private OffsetDateTime expectedDeliveryAt;
+    private OffsetDateTime fulfilledAt;
+
+    private BigDecimal distanceFee;
+    private BigDecimal courierPayout;
+    private BigDecimal totalCost;
+
+    private Integer totalItems;
+
+    private ContactPoint sender;
+    private ContactPoint recipient;
+
+    private List<Item> items = new ArrayList<>();
+
+    public static Delivery draft() {
+        Delivery delivery = new Delivery();
+        delivery.setId(UUID.randomUUID());
+        delivery.setStatus(DeliveryStatus.DRAFT);
+        delivery.setTotalItems(0);
+        delivery.setTotalCost(BigDecimal.ZERO);
+        delivery.setCourierPayout(BigDecimal.ZERO);
+        delivery.setDistanceFee(BigDecimal.ZERO);
+        return delivery;
+    }
+
+    public UUID addItems(String name, Integer quantity) {
+        Item item = Item.brandNew(name, quantity);
+        items.add(item);
+        calculateTotalItems();
+        return  item.getId();
+    }
+
+    public void removeItem(UUID itemId) {
+        items.removeIf(item -> item.getId().equals(itemId));
+    }
+
+    public void changeItemQuantity(UUID itemId, int quantity) {
+        Item item = getItems().stream().filter(i -> i.getId().equals(itemId)).findFirst().orElseThrow();
+        item.setQuantity(quantity);
+        calculateTotalItems();
+    }
+
+    public void removeItems(){
+        items.clear();
+        calculateTotalItems();
+    }
+
+    public void editPrepararionDetails (PreparationDetails details) {
+
+        verifyIfCanBeEdited();
+
+        setSender(details.getSender());
+        setRecipient(details.getRecipiet());
+        setDistanceFee(details.getDistanceFee());
+        setCourierPayout(details.getCourierPayout());
+
+        setExpectedDeliveryAt(OffsetDateTime.now().plus(details.getExpectedDeliveryTime()));
+        setTotalCost(this.getDistanceFee().add(this.getCourierPayout()));
+    }
+
+    public void place() {
+        verifyIfCanBePlaced();
+        this.setStatus(DeliveryStatus.WAITING_FOR_COURIER);
+        this.setPlacedAt(OffsetDateTime.now());
+    }
+
+    public void pickUp(UUID courierId){
+        this.setCourierId(courierId);
+        this.setStatus(DeliveryStatus.IN_TRANSIT);
+        this.setAssignedAt(OffsetDateTime.now());
+    }
+
+    public void marckAsDelivered() {
+        this.setStatus(DeliveryStatus.DELIVERY);
+        this.setFulfilledAt(OffsetDateTime.now());
+    }
+
+    public List<Item> getItems() {
+        return Collections.unmodifiableList(this.items);
+    }
+
+    private void calculateTotalItems(){
+        int totalItems = getItems().stream().mapToInt(Item::getQuantity).sum();
+        setTotalItems(totalItems);
+    }
+
+    private void verifyIfCanBePlaced() {
+        if (!isFilled())
+            throw new DomainException();
+
+        if (!getStatus().equals(DeliveryStatus.DRAFT))
+            throw new DomainException();
+    }
+
+    private void verifyIfCanBeEdited() {
+        if(!getStatus().equals(DeliveryStatus.DRAFT))
+            throw new DomainException();
+    }
+
+    private boolean isFilled() {
+        return this.getSender() != null
+                && this.getRecipient() != null
+                && this.getTotalCost() != null;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @Builder
+    public static class PreparationDetails {
+        private ContactPoint sender;
+        private ContactPoint recipiet;
+        private BigDecimal distanceFee;
+        private BigDecimal courierPayout;
+        private Duration expectedDeliveryTime;
+    }
+
+}
