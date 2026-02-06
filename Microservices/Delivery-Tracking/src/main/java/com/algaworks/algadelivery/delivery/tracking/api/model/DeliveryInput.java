5c61003573c731ac4afcff7b85de18fda9479b92
Luis MX <ing.arellano@gmail.com>
2026-02-06 06:40:39 -0300

    6.1. Implementando endpoint para cadastro de uma Delivery

---
+++ Microservices/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/api/model/DeliveryInput.java
@@ -0,0 +1,28 @@
+package com.algaworks.algadelivery.delivery.tracking.api.model;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class DeliveryInput {
+
+    @NonNull
+    @Valid
+    private ContactPointInput sender;
+
+    @NonNull
+    @Valid
+    private ContactPointInput recipiet;
+
+    @NotEmpty
+    @Valid
+    @Size(min=1)
+    private List<ItemInput> items;
+}
